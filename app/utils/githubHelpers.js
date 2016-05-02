ac0bcdebb01983d3f5987cdab80fb852b2fd4cbc
Gabriel Seixas <gseixas13@hotmail.com>
2016-05-01 21:30:28 -0300

    Fixed get players info method

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -4,14 +4,14 @@ var id = "7de606791248aca82512";
 var sec = "59e8ce4926f95dbe6f27bfd2778692f0cbf95740";
 var param = "?client_id=" + id + "&client_secret=" + sec;
 
-function getUserinfo(username) {
+function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + param);
 }
 
 var helpers = {
   getPlayersinfo: function (players) {
     return axios.all(players.map(function (username) {
-      return getUserinfo();
+      return getUserInfo(username);
     })).then(function (info) {
       return info.map(function (user) {
         return user.data;
