786205fabb81a515b0e191a26420361a5889cb3d
Gabriel Seixas <gseixas13@hotmail.com>
2016-05-03 02:30:11 -0300

    Until class 12

--- app/config/routes.js
+++ app/config/routes.js
@@ -5,19 +5,23 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 
+var hashHistory = ReactRouter.hashHistory;
+
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 var PromptContainer = require('../containers/PromptContainer');
 var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
+var ResultsContainer = require('../containers/ResultsContainer');
 
 var router = (
-  <Router>
+  <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
       <Route path='playerOne' header='Player One' component={PromptContainer} />
       <Route path='playerTwo/:playerOne' header='player Two' component={PromptContainer}/>
       <Route path='battle' component={ConfirmBattleContainer} />
+      <Route path='results' component={ResultsContainer} />
     </Route>
   </Router>
 );
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,6 +8,33 @@ function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + param);
 }
 
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars(repos) {
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count;
+  }, 0);
+}
+
+function getPlayersData(player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      };
+    });
+}
+
+function calculateScores(players) {
+  return players.map(function(player) {
+    return player.followers + 3 + player.totalStars;
+  });
+}
+
 var helpers = {
   getPlayersinfo: function (players) {
     return axios.all(players.map(function (username) {
@@ -19,6 +46,17 @@ var helpers = {
     }).catch(function(err) {
       console.warn('Error in getPlayersinfo', err);
     });
+  },
+
+  battle: function (players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err) {
+        console.warn('Error in getPlayersInfo: ', err);
+      });
   }
 };
 
