af4024b37ed2be92f5f95650a0f575496c947fb6
Gabriel Seixas <gseixas13@hotmail.com>
2016-05-03 13:02:09 -0300

    Change confitm battle logic

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -12,6 +12,43 @@ function puke(object) {
   );
 }
 
+function LoadingMessage(props) {
+  return (
+    <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+      <h1>Loading...</h1>
+    </div>
+  );
+}
+
+function ConfirmBattleComponent(props) {
+  return (
+    <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+      <h1>Confirm players</h1>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <UserDetailsWrapper header='Player 1'>
+          <UserDetails info={props.playersInfo[0]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header='Player 2'>
+          <UserDetails info={props.playersInfo[1]} />
+        </UserDetailsWrapper>
+      </div>
+
+      <div className='col-sm-8 col-sm-offset-2'>
+        <div className='col-sm-12' style={styles.space}>
+          <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
+            Initiate Battle!
+          </button>
+        </div>
+        <div className='col-sm-12' style={styles.space}>
+          <Link to='/playerOne'>
+            <button type='button' className='btn btn-lg btn-danger'> Reselect Players </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 ConfirmBattle.PropTypes = {
   isLoading: PropTypes.bool.isRequired,
   onInitiateBattle: PropTypes.func.isRequired,
@@ -21,38 +58,12 @@ ConfirmBattle.PropTypes = {
 function ConfirmBattle (props) {
   var element;
   if (props.isLoading) {
-    element = (
-      <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
-        <h1>Loading...</h1>
-      </div>
-    );
+    element = <LoadingMessage />
   } else {
-    element = (
-      <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
-        <h1>Confirm players</h1>
-        <div className='col-sm-8 col-sm-offset-2'>
-          <UserDetailsWrapper header='Player 1'>
-            <UserDetails info={props.playersInfo[0]} />
-          </UserDetailsWrapper>
-          <UserDetailsWrapper header='Player 2'>
-            <UserDetails info={props.playersInfo[1]} />
-          </UserDetailsWrapper>
-        </div>
-
-        <div className='col-sm-8 col-sm-offset-2'>
-          <div className='col-sm-12' style={styles.space}>
-            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
-              Initiate Battle!
-            </button>
-          </div>
-          <div className='col-sm-12' style={styles.space}>
-            <Link to='/playerOne'>
-              <button type='button' className='btn btn-lg btn-danger'> Reselect Players </button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
+    element = <ConfirmBattleComponent
+                  isLoading={props.isLoading}
+                  onInitiateBattle={props.onInitiateBattle}
+                  playersInfo={props.playersInfo} />
   }
   return element;
 }
